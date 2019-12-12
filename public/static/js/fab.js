df4365c36d651c305ee4515b57b0858340ee0a91
KevinThePepper <kevin.shelley@pm.me>
2019-12-12 17:57:15 -0500

    Updated files to make theme change more efficient

--- public/static/js/fab.js
+++ public/static/js/fab.js
@@ -1,19 +1,37 @@
 const LIGHT_THEME = 'light';
 const DARK_THEME = 'dark';
 
-function bgOnLoad() {
+/**
+ * Returns the theme obtained from local storage. If the theme is invalid, then
+ * the theme is defaulted to light.
+ *
+ * @returns {string} The theme stored in local storage.
+ */
+function getValidThemeFromStorage() {
     let theme = localStorage.getItem('docs-theme') || LIGHT_THEME;
-    $('head link#theme').attr('href', 'static/css/' + theme + '.css');
+    return (theme === LIGHT_THEME || theme === DARK_THEME) ? theme : LIGHT_THEME;
 }
 
-function fabOnClick() {
-    // get theme from local storage
-    let theme = localStorage.getItem('docs-theme') || LIGHT_THEME;
-    if(theme !== LIGHT_THEME && theme !== DARK_THEME) {
-        console.warn('Invalid docs-theme storage found. Defaulting to light theme.');
+/**
+ * Checks the theme that should be loaded. If the theme that should be loaded is the dark one,
+ * then the theme is switched.
+ */
+function bgOnLoad() {
+    let theme = getValidThemeFromStorage();
+    let currentTheme = $('head link#theme').attr('href');
+    let storedTheme = 'static/css/' + theme + '.css';
+    if(currentTheme !== storedTheme) {
+        $('head link#theme').attr('href', 'static/css/' + theme + '.css');
     }
+}
+
+/**
+ * Handles the change of theme. If the theme in local storage is not a valid theme, then
+ * the theme is defaulted to light.
+ */
+function fabOnClick() {
+    let theme = getValidThemeFromStorage();
     let newTheme = (theme === DARK_THEME) ? LIGHT_THEME : DARK_THEME;
     $('head link#theme').attr('href', 'static/css/' + newTheme + '.css');
     localStorage.setItem('docs-theme', newTheme);
-    location.reload();
 }
