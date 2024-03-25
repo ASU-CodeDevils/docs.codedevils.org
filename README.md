087c56b189b0db1b96e50b25ab651013334a57dc
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-25 07:09:13 -0700

    initial commits

---
+++ README.md
@@ -0,0 +1,21 @@
+# CodeDevils' Official Documentation
+
+This is CodeDevils' official documentation for all of our projects. We are using [Nextra](https://nextra.site) to build this documentation.
+
+[![](.github/screenshot.png)](https://nextra-docs-template.vercel.app)
+
+## Quick Start
+
+Click the button to clone this repository and deploy it on Vercel:
+
+[![](https://vercel.com/button)](https://vercel.com/new/clone?s=https%3A%2F%2Fgithub.com%2Fshuding%2Fnextra-docs-template&showOptionalTeamCreation=false)
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import Footer from './components/Footer';
+import Link from 'next/link';
+// import Footer from '@sloppyflipflop/react-components-library';
+
+const config: DocsThemeConfig = {
+  logo: <h1>docs.codedevils</h1>,
+  head: (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta property='og:title' content="Codedevils' Documenation" />
+      <meta
+        property='og:description'
+        content='Documentation for the CodeDevils organization'
+      />
+      <script src='https://cdn.tailwindcss.com'></script>
+    </>
+  ),
+  project: {
+    // CodeDevils github organization page
+    link: 'https://github.com/ASU-CodeDevils',
+  },
+  chat: {
+    // CodeDevils Discord server
+    link: 'https://discord.gg/eevXKjVmm2',
+  },
+  docsRepositoryBase: 'https://github.com/ASU-CodeDevils/docs.codedevils.org',
+  footer: {
+    // component: <Footer />,
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <Link
+          href='https://codedevils.org'
+          target='_blank'
+          className='hover:underline hover:text-maroon transition-colors duration-200 ease-in-out'
+        >
+          CodeDevils
+        </Link>
+        .
+      </span>
+    ),
+  },
+
+  /*
+  Sorry for the late ping @Officer. I just got done with the sponsorship packet. These will go out tomorrow around noon. If you happen to have time to look it over in the morning, I would appreciate some feedback. Thanks and sweet dreams! 😊
+
+
+  This is my diary, so that I can shit talk and the such.
+
+  */
+};
+
+export default config;
